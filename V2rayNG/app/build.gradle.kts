e09a847098e4e03b5ebc8adb2ac427410be10d2f
tragedy <nasirimadkid@Gmail.com>
2026-06-01 10:09:06 +0330

    Change package namespace and application ID

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.v2ray.ang"
+    namespace = "com.asuray.ang"
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.asuray.ang"
         minSdk = 24
         targetSdk = 37
         versionCode = 731
